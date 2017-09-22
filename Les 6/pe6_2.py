fb3882c8631863b657a3c35ad77d1ddacdfd1de4
Luuk <luukhendriks13>
2017-09-22 14:43:42 +0200

    Alle opdrachten

--- Les 6/pe6_2.py
+++ Les 6/pe6_2.py
@@ -1,1 +1,8 @@
 aantal = eval(input('Geef lijst met minimaal 10 strings: '))
+list = []
+for woord in aantal:
+    if len(woord) == 4:
+        list.append(woord)
+
+print('De nieuw-gemaakte lijst met alle vier-letter strings is:', list)
+
