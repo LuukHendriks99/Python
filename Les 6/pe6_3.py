6beb0c92fcc185208b0b21312b5d5812fde0dbde
Luuk <luukhendriks13>
2017-09-26 12:04:59 +0200

    Alle opdrachten

--- Les 6/pe6_3.py
+++ Les 6/pe6_3.py
@@ -1,10 +1,10 @@
 nummers = "5-9-7-1-7-8-3-2-4-8-7-9"
-
 list = []
 for cijfers in nummers:
-    if type(nummers) == int or float:
-        list.append(cijfers)
-    else:
-        cijfers.strip()
+    if cijfers in '0123456789':
+        list.append(int(cijfers))
 
-print(list)
+print('Gesorteerde list van ints:', sorted(list))
+print('Grootste getal:', max(list), 'en Kleinste getal:', min(list))
+print('Aantal getallen:', len(list), 'en Som van de getallen:', sum(list))
+print('Gemiddelde:', sum(list) / len(list))
