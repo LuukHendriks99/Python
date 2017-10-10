d3742f2989d31deee3170460253b590e7c2e755c
Luuk <luukhendriks13>
2017-10-10 13:50:11 +0200

    Alle opdrachten

--- Les 9/pe9_2.py
+++ Les 9/pe9_2.py
@@ -1,21 +1,22 @@
 import time
-import csv
 
 bestand = 'inloggers.csv'
 
+
 def inloggen():
-    with open(bestand, 'w') as f:
+    with open(bestand, 'a') as f:
         while True:
-            tijd = time.strftime('%a %d %b %Y at %H:%M', time.localtime())
+
             naam = input('Wat is je achternaam? ')
+            if naam == 'einde' or naam == 'Einde':
+                break
+            tijd = time.strftime('%a %d %b %Y at %H:%M', time.localtime())
             voorl = input('Wat zijn je voorletters? ')
             gbdatum = input('Wat is je geboortedatum? ')
             email = input('Wat is je e-mail adres? ')
-
-            gegevens = tijd + naam + voorl + gbdatum + email
+            gegevens = tijd + ';' + naam + ';' + voorl + ';' + gbdatum + ';' + email
             f.write(gegevens)
             f.write('\n')
-            if naam == 'einde' or 'Einde':
-                break
+
 
 inloggen()
